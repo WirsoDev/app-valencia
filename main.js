ebe20b44c0bff065293bad7d06c814e59608a66c
wirsodev <wilson.wmarques@gmail.com>
2023-09-18 15:54:18 +0100

    add cards contacts

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 import './styles/index.css'
 import './styles/home.css'
 import './styles/alertbox.css'
+import './styles/contacts.css'
 import { createBalls, clickBallsHandler, ballsParalax, requestDeviceOrientation} from './balls'
 import { mainBtn } from './btnshandler'
 import { emailMain } from './emailHandler'
 import {crateAlertBox} from './alertBox'
+import { contactsMain } from './contactsHandler'
+import vscards from 'vcards-js'
+
 
 const container = document.querySelector('.app-balls')
-
-//test IOS
-
 
 
 function main(){
@@ -19,6 +20,7 @@ function main(){
     mainBtn()
     emailMain()
     requestDeviceOrientation()
+    contactsMain()
 }
 
 main()
